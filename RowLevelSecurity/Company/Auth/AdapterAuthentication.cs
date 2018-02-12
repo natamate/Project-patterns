a861471e6fdddf50a87b245ccbad806f38576bab
Minh Nhat Trinh <32866610+MinhNhat007@users.noreply.github.com>
2018-02-12 16:37:42 +0100

    Update adaper for authentication

---
+++ RowLevelSecurity/Company/Auth/AdapterAuthentication.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ACLDatabase.Model;
+
+namespace ACLDatabase.Company.Auth
+{
+    public class AdapterAuthentication<T>: OldAuthentication<T>, Authentication<T> where T: ModelContext
+    {
+        public void Authenticate(string userName, T context)
+        {
+            MakeAuthentication(userName, context);
+        }
+    }
+}
