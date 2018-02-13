771b16e687697c70208756fbaf72c4b1d1f56a7e
marcello96 <jakubowskimarcin96@gmail.com>
2018-02-13 13:02:03 +0100

    conditional initialization added

--- RowLevelSecurity/Company/MainClass.cs
+++ RowLevelSecurity/Company/MainClass.cs
@@ -1,11 +1,34 @@
 ﻿using ACLDatabase.Company.DB;
 using System.Data.Entity;
 using ACLDatabase.UI;
+using System;
 
 namespace ACLDatabase.Company
 {
     public class MainClass
     {
+        public static void Initialization()
+        {
+            Console.Clear();
+            Console.WriteLine("Should drop and make default initialization?");
+            Console.WriteLine("0. No");
+            Console.WriteLine("1. Yes");
+            var res = Console.ReadLine();
+            switch (res)
+            {
+                case "0":
+                    return;
+                case "1":
+                    Database.SetInitializer(new InitializeData());
+                    return;
+                default:
+                    Initialization();
+                    return;
+            }
+
+
+        }
+
         public static void Main(string[] args)
         {
             //Main program
@@ -14,7 +37,8 @@ namespace ACLDatabase.Company
                 //Declaration for UI, Database, Model and IAuthentication
                 var drawingStrategy = new DrawEmployeesFinancy();
                 var myUi = new ConsoleUi(myDb, drawingStrategy);
-                Database.SetInitializer(new InitializeData());
+                
+                Initialization();
                 var controller = new CompanyController<CompanyContext>(myUi, myDb);
 
                 while (true)
